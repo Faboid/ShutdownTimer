dc406c1d23b7c745fa94a30ed836a2d8d7f86e63
Faboid <fabio.meloni.2000@tiscali.it>
2022-01-02 22:14:10 +0100

    Created TimeHandler class, a class to move certain logic away from the UI.

---
+++ AutomaticShutdownTimerLibrary/TimeHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace AutomaticShutdownTimerLibrary {
+    public class TimeHandler {
+
+        public event EventHandler<string> SecondHasPassed;
+
+        private readonly Time time;
+        private readonly Timer timer = new Timer(1000); // one second
+
+        public TimeHandler(int hours, int minutes, int seconds) {
+            time = new Time(hours, minutes, seconds);
+            timer.Elapsed += SecondPassed;
+        }
+
+        private void SecondPassed(object sender, ElapsedEventArgs e) {
+            time.SubtractOneSecond();
+            SecondHasPassed?.Invoke(this, time.ToString());
+        }
+
+        public void Start() {
+            timer.Start();
+        }
+
+        public void Stop() {
+            timer.Stop();
+        }
+
+    }
+}
