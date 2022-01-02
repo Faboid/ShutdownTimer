8120aed7a7abf6782f89ed64f29a23f9d7a6bdc6
Faboid <fabio.meloni.2000@tiscali.it>
2022-01-02 19:31:58 +0100

    Added test project to make sure the behavior stays consistent while the refactoring gets done.

---
+++ AutomaticShutdownTimerTests/TimeTests.cs
@@ -0,0 +1,51 @@
+﻿using AutomaticShutdownTimerLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AutomaticShutdownTimerTests {
+    public class TimeTests {
+    
+    
+        [Theory]
+        [InlineData("02 : 15 : 03", 2, 15, 3)]
+        [InlineData("12 : 00 : 00", 12, 0, 0)]
+        [InlineData("00 : 00 : 09", 0, 0, 9)]
+        public void CorrectFormat_ToString(string expected, int hours, int minutes, int seconds) {
+
+            //arrange
+            Time time = new Time(hours, minutes, seconds);
+            string actual;
+
+            //act
+            actual = time.ToString();
+
+            //assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Theory]
+        [InlineData("02 : 15 : 02", 2, 15, 3)]
+        [InlineData("11 : 59 : 59", 12, 0, 0)]
+        [InlineData("00 : 00 : 08", 0, 0, 9)]
+        public void SubtractOneSuccessfully(string expected, int hours, int minutes, int seconds) {
+
+            //arrange
+            Time time = new Time(hours, minutes, seconds);
+            string actual;
+
+            //act
+            time.SubtractOneSecond();
+            actual = time.ToString();
+
+            //assert
+            Assert.Equal(expected, actual);
+
+        }
+
+    }
+}
