a76924bc64f190708eb2e6c2d9efcf9ffc310d04
Faboid <fabio.meloni.2000@tiscali.it>
2022-01-04 22:36:42 +0100

    Implemented tests for the Alarm class.

---
+++ AutomaticShutdownTimerTests/AlarmTests.cs
@@ -0,0 +1,72 @@
+﻿using AutomaticShutdownTimerLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AutomaticShutdownTimerTests {
+    public class AlarmTests {
+
+        event EventHandler<EventArgs> randomEvent;
+
+        [Fact]
+        public void SingleEventCall() {
+
+            //arrange
+            int count = 0;
+            int expected = 1;
+            randomEvent += (s, e) => { count++; };
+            var alarm = new Alarm(randomEvent, 30, true);
+
+            //act
+            alarm.Invoke();
+            alarm.Invoke();
+
+            //assert
+            Assert.Equal(expected, count);
+
+        }
+
+        [Fact]
+        public void MultipleEventCall() {
+
+            //arrange
+            int count = 0;
+            int expected = 3;
+            randomEvent += (s, e) => { count++; };
+            var alarm = new Alarm(randomEvent, 30, false);
+
+            //act
+            alarm.Invoke();
+            alarm.Invoke();
+            alarm.Invoke();
+
+            //assert
+            Assert.Equal(expected, count);
+
+        }
+
+        [Fact]
+        public void ResetEventCall() {
+
+            //arrange
+            int count = 0;
+            int expected = 2;
+            randomEvent += (s, e) => { count++; };
+            var alarm = new Alarm(randomEvent, 30, true);
+
+            //act
+            alarm.Invoke();
+            alarm.Reset();
+            alarm.Invoke();
+            alarm.Invoke();
+
+            //assert
+            Assert.Equal(expected, count);
+
+        }
+
+    }
+}
